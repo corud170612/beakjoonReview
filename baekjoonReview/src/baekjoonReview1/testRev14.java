5b9947711fac139ba5ed77413aa23a7042aff2a0
chae <chae@DESKTOP-2HR31HA>
2022-05-15 15:59:45 +0900

    2022.05.15 단계별로 풀어보기 입출력 부분 풀기

---
+++ baekjoonReview/src/baekjoonReview1/testRev14.java
@@ -0,0 +1,19 @@
+package baekjoonReview1;
+//단계별로 풀어보기
+//입출력과 사칙연산 단계
+//25083 새싹
+public class testRev14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |";
+		
+		System.out.println(s);
+	}
+
+}
---
+++ baekjoonReview/src/baekjoonReview4/testRev11.java
@@ -0,0 +1,30 @@
+package baekjoonReview4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//단계별로 풀어보기 반복문 재복습
+
+//10871번 X보다 작은 수
+public class testRev11 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
